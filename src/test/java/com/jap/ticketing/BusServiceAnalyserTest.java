7bac771cae6e1af9e7f7d5a7558a13d765785c96
Nikita Chauhan <nikitachauhan64@gmail.com>
2022-09-13 10:29:03 +0530

    created the bus service analyser test class

---
+++ src/test/java/com/jap/ticketing/BusServiceAnalyserTest.java
@@ -0,0 +1,12 @@
+/*
+ *Author Name: Nikita Chauhan
+ *Date: 13-09-2022
+ *Created With: IntelliJ IDEA Community Edition
+ */
+
+
+package com.jap.ticketing;
+
+public class BusServiceAnalyserTest {
+
+}
